d2548e339f19b97b9bcf6bc9d9b909030f19823f
Matheus Amaro <matheus.amaro@gmail.com>
2016-09-08 13:23:13 -0300

    Enhancements.

---
+++ src/ashttp/AsHttpResponse.java
@@ -0,0 +1,11 @@
+package ashttp;
+
+import java.io.Serializable;
+
+public class AsHttpResponse implements Serializable {
+	private static final long serialVersionUID = 8943293685945666320L;
+	
+	public AsHttpResponse() {
+		
+	}
+}
