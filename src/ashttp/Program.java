e6a34687df7644a358361f352832a25c6b93a607
Matheus Amaro <matheus.amaro@gmail.com>
2017-04-19 16:22:57 -0300

    Fix - Amarocorp request page

--- src/ashttp/Program.java
+++ src/ashttp/Program.java
@@ -1,15 +1,12 @@
 package ashttp;
 
-import ashttp.models.AsHttpAuthorization;
 import ashttp.models.enumtypes.AsEnumMethod;
 
 public class Program {
 	public static void main(String[] args) {
 		AsHttpRequest request = new AsHttpRequest();
-		request.setUrl("http://sapdsf.grupomag.corp:8000/gerencial/");
-		request.setMethod(AsEnumMethod.GET);
-		request.setAuthorization(new AsHttpAuthorization("phlp0001", "3141569"));
-		
+		request.setUrl("http://amarocorp.com.br/");
+		request.setMethod(AsEnumMethod.GET);		
 		try {
 			request.send();
 			
